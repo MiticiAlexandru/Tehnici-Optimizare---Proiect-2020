1fe145b5d28c7848b0fd1b9823af7f544962a8f6
David Irimia <36113284+daverave1212@users.noreply.github.com>
2020-05-10 16:55:06 +0300

    Fixed some documentation - implemetation difference issues

--- Algoritm 2 - Gradient Descent/gradient_descent.py
+++ Algoritm 2 - Gradient Descent/gradient_descent.py
@@ -1,5 +1,4 @@
 import numpy
-import random
 import matplotlib.pyplot as pyplot
 import pandas
 import json
@@ -10,7 +9,7 @@ import time
 def get_points():   # Get the points from Datasets/points.json
     dir_path = path.dirname(path.realpath(__file__))
     datasets_folder = path.join(dir_path, 'Datasets')
-    points_path = path.join(datasets_folder, 'stochastic_points.json')
+    points_path = path.join(datasets_folder, 'points.json')
     points = None
     with open(points_path, 'r') as file:
         points = json.loads(file.read())
@@ -32,7 +31,7 @@ def plot_line(slope, intercept):
 
 
 #########################################################################
-############################ Stochastic Gradient Descent ###########################
+############################ Gradient Descent ###########################
 #########################################################################
 
 # Uses the Least Squares algorithm/formula to find the optimal slope for Linear Regression.
@@ -55,7 +54,7 @@ def get_slope_by_least_squares(points):
 
 
 
-def stochastic_gradient_descent_for_intercept(points, m, show=False):
+def gradient_descent_for_intercept(points, m, show=False):
 
     '''
     Bazat pe ecuatia dreptei.
@@ -66,34 +65,62 @@ def stochastic_gradient_descent_for_intercept(points, m, show=False):
     def predict_y(x, c):
         return m * x + c
 
-   
-    def stochastic_loss(c):
-        r = random.choice(points)
-        return 
-
+    '''
+    Eroarea prezicerii functiei de mai sus.
+    Cu cat mai aproape de 0, cu atat mai bine.
+    '''
+    def residual_error(point, c):
+        return point['y'] - predict_y(point['x'], c)
 
     '''
-    Obtinem panta printr-un calcul simplu.
+    Functia loss este o parabola, un polinom de gradul 2 in functie de c.
+    Minimul acesei functii inseamna cel mai bun c, astfel incat erorile totale
+      sa fie cat mai mici.
     '''
-    def stochastic_loss(c):
-        r = random.choice(points)
-        return (-2) * (r['y'] - c - m * r['x'])
+    def loss(c):
+        return sum([ residual_error(point, c)**2 for point in points ])
+
+    '''
+    Din cauza ca loss function este de gradul 2, derivata este de gradul 1,
+      adica o dreapta.
+    Derivata intr-un punct de pe loss function are o panta.
+    Cu cat panta se apropie de 0, cu atat inseamna ca ne apropiem de minimul functiei loss.
+    '''
+    def loss_deriv(c):
+        return sum([ (-2)*residual_error(point, c) for point in points ])
 
     ######################### Algoritm #####################
 
     '''
+    Optional, putem pune pe grafic si graficul functiei loss
+    '''
+    def plot_helpers():
+        cs = numpy.linspace(-5, 5, 100)
+        losses = [loss(c) for c in cs]
+        plot_points([{
+            'x' : cs[i],
+            'y' : losses[i]
+        } for i in range(len(cs))], False)
+
+        losses = [loss_deriv_slope(c) for c in cs]
+        plot_points([{
+            'x' : cs[i],
+            'y' : losses[i]
+        } for i in range(len(cs))], False, 'ro')
+    
+
+    '''
     Pornim cu c de la o valoare arbitrara in 'stanga' punctului de minim al functiei loss.
-    La fiecare iteratie, loss functionul devine din ce in ce mai mic.
-    Ca urmare, c devine din ce in ce mai aproape de solutie.
+    La fiecare iteratie, ne vom apropia cu panta derivatei de 0.
+    Folosim aceasta panta ca sa ajustam interceptul (c).
     Ne oprim cand am ajuns foarte aproape de 0.
     '''
     c = 0
     while True:
-        step_size = stochastic_loss(c) * 0.005
-        c = c - step_size
-        print(c, step_size)
-        if abs(step_size) < 0.0001:
+        print(c, loss_deriv(c))
+        if abs(loss_deriv(c)) < 0.1:    # Verificăm dacă ne-am apropiat suficient de tare
             break
+        c = c - loss_deriv(c) * 0.01        
         time.sleep(0.2)
         
     
@@ -110,7 +137,7 @@ pyplot.xlim(0, 10)
 
 plot_points(points, show=False)
 m = get_slope_by_least_squares(points)
-b = stochastic_gradient_descent_for_intercept(points, m)
+b = gradient_descent_for_intercept(points, m)
 
 plot_line(m, b)
 pyplot.show()
